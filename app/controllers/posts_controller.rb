a2395471b3836cc84d5b8a80dda2adf5058861c0
EC2 Default User <ec2-user@ip-172-31-53-160.ec2.internal>
2020-10-13 03:02:28 +0000

    [Add]

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_path(@post.id)
+  end
+
+  def index
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+    
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:tweet, :image)
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users, contorollers: {
+       sessions: 'users/sessions',registrations: 'user/registration'
+     }
+
   root 'home#top'
   get 'home/about'
+  resources :posts, only: [:new, :create, :index, :show]
+  resources :users, only: [:new, :show, :index, :update, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
